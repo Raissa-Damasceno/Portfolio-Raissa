32b7ba039cf5023f57efce6ca0bc2cc61e2bc28c
Raissa Damasceno <raissa.cod@gmail.com>
2022-06-02 15:10:36 +0100

    first commit

--- src/Components/PortfolioContainer/Projects/Projects.js
+++ src/Components/PortfolioContainer/Projects/Projects.js
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import ScreenHeading from "../../utilities/ScrennHeading/ScreenHeading";
+import projectsJson from "../../../projects.json";
 
-import './Projects.css'
+import "./Projects.css";
 
 function Projects(props) {
-    return ( 
-        <div className="projects-container screen-container" id={props.id || ""}>
+  const [projects, setProjects] = useState(projectsJson);
+
+  return (
+    <div className="projects-container screen-container" id={props.id || ""}>
       <div className="projects-parent">
         <ScreenHeading title={"Projects"} subHeading={"My Projects"} />
+        <div className="projects--list">
+        {projects.map(
+          (project) => (
+            <div className='projects--card'>
+            <div className="projects--name">{project.name} </div>
+            <div className="projects--type">{project.type} </div>
+            <div className="projects--img">
+            <img src={project.image} width={300} alt="No Connection"></img>
+            </div>
+            <div className="projects--language">{project.language} </div>
+            <div className="projects--description">{project.description} </div>
+            <a href={project.link} className="projects--link" target="_blank" rel="noreferrer" > Repositório </a>
+            </div>
+          )
+        )}
+        </div>
       </div>
     </div>
-     );
+  );
 }
 
 export default Projects;
